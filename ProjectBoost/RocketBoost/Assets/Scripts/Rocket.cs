0fe53d89aa1b55c7bb36239cd607f2f7bd13a009
Carter Ithier <ithier@Carters-MacBook-Pro.local>
2019-08-31 21:38:48 -0400

    added second level and sound effects

--- ProjectBoost/RocketBoost/Assets/Scripts/Rocket.cs
+++ ProjectBoost/RocketBoost/Assets/Scripts/Rocket.cs
@@ -1,62 +1,97 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     // config params
     [SerializeField] float mainThrust = 800f;
     [SerializeField] float rcsThrust = 100f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip death;
+    [SerializeField] AudioClip finish;
+
+    enum State {Alive, Dying, Transcending};
+    State state = State.Alive;
 
     // cached references
     Rigidbody rigidBody;
     AudioSource audioSource;
+    int currentScene;
+    int nextScene;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        nextScene = currentScene + 1 > SceneManager.sceneCount ? currentScene : currentScene + 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // ignore collisions if not alive
+        if (state != State.Alive) {return;}
+
         switch (collision.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("OK");
                 break;
-            case "Fuel":
-                Debug.Log("Fuel");
+            case "Finish":
+                BeginSuccessSequence();
                 break;
             default:
-                Debug.Log("Dead");
+                BeginDeathSequence();
                 break;
         }
     }
 
-    private void Thrust()
+    private void BeginSuccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finish);
+        Invoke("LoadNextScene", 1f);
+    }
+
+    private void BeginDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        Invoke("LoadStartScene", 1f);
+    }
+
+    private void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+
+    private void RespondToThrustInput()
     {
         // GetKey works all the time while getkeydown only applies when it first goes down
         // can thrust while rotating
         if (Input.GetKey(KeyCode.Space))
         {
-            float thrustThisFrame = mainThrust * Time.deltaTime;
-
-            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
-
-            if (!audioSource.isPlaying)
-            {
-                audioSource.Play();
-            }
+            ApplyThrust();
 
         }
         else
@@ -65,7 +100,19 @@ public class Rocket : MonoBehaviour
         }
     }
 
-    private void Rotate()
+    private void ApplyThrust()
+    {
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+
+        rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+    }
+
+    private void RespondToRotateInput()
     {
         // take manual control of rotation
         rigidBody.freezeRotation = true;
